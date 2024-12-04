935e9b43846e5811df87fdf3de76667c7443e169
tanzid109 <tanzid.unkhalil109@gmail.com>
2024-12-04 11:21:32 +0600

    firebase added

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaHAnYU4zQP-L0bvpc_1nsXdhWCfTZzYs",
+  authDomain: "movie-portal-a7883.firebaseapp.com",
+  projectId: "movie-portal-a7883",
+  storageBucket: "movie-portal-a7883.firebasestorage.app",
+  messagingSenderId: "927331404752",
+  appId: "1:927331404752:web:0cc432d8f9d02b08d607d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/routes/router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <p>hello</p>
+    },
+]);
+export default router
